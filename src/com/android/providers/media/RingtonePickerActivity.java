b64e0699f5539a161973fb20bf78fca112629873
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-11-10 21:49:13 +0200

    RingtonePickerActivity: Add Color engine support
    Change-Id: Iaaf1bf8cb7c3e4cc852bf5eef278b5e13ecee16d

--- src/com/android/providers/media/RingtonePickerActivity.java
+++ src/com/android/providers/media/RingtonePickerActivity.java
@@ -16,6 +16,9 @@
 
 package com.android.providers.media;
 
+import android.app.IThemeCallback;
+import android.app.ThemeManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -122,6 +125,9 @@ public final class RingtonePickerActivity extends AlertActivity implements
      */
     private static Ringtone sPlayingRingtone;
 
+    private int mTheme;
+    private ThemeManager mThemeManager;
+
     private DialogInterface.OnClickListener mRingtoneClickListener =
             new DialogInterface.OnClickListener() {
 
@@ -144,9 +150,40 @@ public final class RingtonePickerActivity extends AlertActivity implements
 
     };
 
+    private final IThemeCallback mThemeCallback = new IThemeCallback.Stub() {
+
+        @Override
+        public void onThemeChanged(int themeMode, int color) {
+            onCallbackAdded(themeMode, color);
+            RingtonePickerActivity.this.runOnUiThread(() -> {
+                RingtonePickerActivity.this.recreate();
+            });
+        }
+
+        @Override
+        public void onCallbackAdded(int themeMode, int color) {
+            mTheme = color;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final int themeMode = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.THEME_PRIMARY_COLOR, 0);
+        final int accentColor = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.THEME_ACCENT_COLOR, 0);
+        mThemeManager = (ThemeManager) getSystemService(Context.THEME_SERVICE);
+        if (mThemeManager != null) {
+            mThemeManager.addCallback(mThemeCallback);
+        }
+        if (themeMode != 0 || accentColor != 0) {
+            getTheme().applyStyle(mTheme, true);
+        }
+        if (themeMode == 1 || themeMode == 3) {
+            getTheme().applyStyle(R.style.PickerDialogTheme_Dark, true);
+        }
 
         mHandler = new Handler();
 
